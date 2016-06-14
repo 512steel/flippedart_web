9129f7d5ec6df1318992214386d5b6d8c153e783
petestreet <petestreet01@gmail.com>
2016-06-13 22:04:08 -0500

    foundation wip #8

--- imports/ui/pages/home-page.js
+++ imports/ui/pages/home-page.js
@@ -1,1 +1,21 @@
+import { Template } from 'meteor/templating';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
 import './home-page.html';
+
+
+Template.home_page.onRendered(function() {
+
+    Meteor.setTimeout(function(){
+        var el1 = new Foundation.Equalizer($('#home-equalized-1'));
+        var el2 = new Foundation.Equalizer($('#home-equalized-2'));
+    }, 150);
+});
+
+Template.home_page.helpers({
+    currrentUsername: function() {
+        if (Meteor.user()) {
+            return Meteor.user().username;
+        }
+    }
+});
